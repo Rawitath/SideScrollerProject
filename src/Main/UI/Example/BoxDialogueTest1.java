f1207f86161a7596e6f51e202c0b738acbbd01c3
Niravit-Kajonbunjong <67070245@kmitl.ac.th>
2025-03-25 12:55:49 +0700

    Make dialogue.(incomplete)

---
+++ src/Main/UI/Example/BoxDialogueTest1.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main.UI.Example;
+
+/**
+ *
+ * @author nirawith2548gmail.com
+ */
+import Entities.UI.UIImage;
+import Scenes.Scene;
+import Datas.Vector2;
+import Saves.GameSave;
+
+public class BoxDialogueTest1 extends UIImage{
+    private ScriptDialogueTest1 sd1;
+    private GameSave gs;
+    
+    public BoxDialogueTest1(Scene s){
+        super(s);
+        sd1 = new ScriptDialogueTest1(s);
+        this.addChild(this.sd1);
+        this.setPosition(new Vector2(0 , 0));
+    }
+
+    @Override
+    public void start() {
+        
+    }
+
+    @Override
+    public void update() {
+        gs.cutscene[0] = true;
+    }
+
+    @Override
+    public void fixedUpdate() {
+        
+    }
+    
+}
