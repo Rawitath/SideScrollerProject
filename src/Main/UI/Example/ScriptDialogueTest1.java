eba0b7a234c29bf0a0b03c5e71921d077e6a30f2
Niravit-Kajonbunjong <67070245@kmitl.ac.th>
2025-03-25 12:55:58 +0700

    Make dialogue.(incomplete)

---
+++ src/Main/UI/Example/ScriptDialogueTest1.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main.UI.Example;
+
+/**
+ *
+ * @author nirawith2548gmail.com
+ */
+import Entities.UI.UIText;
+import Scenes.Scene;
+
+public class ScriptDialogueTest1 extends UIText{
+    
+    public ScriptDialogueTest1(Scene s){
+        super(s);
+//        try{
+            this.setText("Hello World!!");
+            this.setLocalPosition(UIText.CENTER);
+            //this.wait(3000);
+//        }
+//        catch(InterruptedException ie){
+//            ie.getStackTrace();
+//        }
+        
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void fixedUpdate() {
+    }
+    
+    
+}
