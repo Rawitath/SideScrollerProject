c0d9448d7b237c7fec8e54cd31a3071f1d78963c
Niravit-Kajonbunjong <67070245@kmitl.ac.th>
2025-03-27 12:02:31 +0700

    Add dialogue.

--- src/Main/UI/Example/ScriptDialogueTest1.java
+++ src/Main/UI/Example/ScriptDialogueTest1.java
@@ -10,29 +10,38 @@ package Main.UI.Example;
  */
 import Entities.UI.UIText;
 import Scenes.Scene;
+import Physics.Time;
 
 public class ScriptDialogueTest1 extends UIText{
-    
+    private float start = Time.time();
+    private float duration = 3f;
+    private String[] dialogue = {"Hello World!" , "New message."};
+    private byte dialoguePick = 0;
+
     public ScriptDialogueTest1(Scene s){
         super(s);
-//        try{
-            this.setText("Hello World!!");
-            this.setLocalPosition(UIText.CENTER);
-//            sleep(3000);
-            this.setText("New message.");
-//        }
-//        catch(InterruptedException ie){
-//            ie.getStackTrace();
-//        }
-        
+        this.setLocalPosition(UIText.CENTER);
     }
-
+    
+    public byte getLastDialogue(){
+        return (byte) (dialogue.length - 1);
+    }
     @Override
     public void start() {
     }
 
     @Override
     public void update() {
+        if (Time.time() - start < duration){
+            this.setText(dialogue[dialoguePick]);
+        }
+        else{
+            this.start = Time.time();
+            if (this.dialoguePick < this.getLastDialogue()){
+                dialoguePick +=1;
+            }
+        }
+        
     }
 
     @Override
