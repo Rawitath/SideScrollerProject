cfef505a6f79af2bf067a8ef237b31cf72c35da9
Niravit-Kajonbunjong <67070245@kmitl.ac.th>
2025-03-28 22:02:42 +0700

    Add text.

--- src/Main/StartMenu/Entities/Option/HeaderFrame.java
+++ src/Main/StartMenu/Entities/Option/HeaderFrame.java
@@ -7,6 +7,7 @@ package Main.StartMenu.Entities.Option;
 import Datas.Vector2;
 import Entities.UI.UIImage;
 import Main.StartMenu.Entities.Fadable;
+import Main.StartMenu.Entities.TextButton;
 import Scenes.Scene;
 import Utilities.FileReader;
 
@@ -15,11 +16,17 @@ import Utilities.FileReader;
  * @author GA_IA
  */
 public class HeaderFrame extends UIImage implements Fadable{
-
+    private TextButton header;
     public HeaderFrame(Scene s) {
         super(s);
         setImage(FileReader.readImage("res/game/settingsmenu/Border1.png"));
         setScale(new Vector2(getImage().getWidth(), getImage().getHeight()));
+        
+        header = new TextButton(s);
+        header.setText("Settings");
+        this.addChild(header);
+        header.setSize(100);
+        header.setLocalPosition(new Vector2(-130,20));
     }
     @Override
     public void start() {
